2409b37385120be5296c17d521f584a44763e78d
sadikcoban <coban.sadik@icloud.com>
2022-09-06 16:18:11 +0300

    first commit

---
+++ MyMessenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMessenger
+//
+//  Created by Sadık Çoban on 6.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
